f43e6198359263be786151c33e4e66841ab60427
Shota Iguchi <shota-iguchi@cookpad.com>
2016-08-25 03:31:23 +0900

    Add test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Post do
   let!(:post) { Post.create(title: 'title', content: 'content') }
-  before { post.comments.create(content: 'comment content') }
+
+  before do
+    post.comments.create(content: 'comment content 1')
+    post.comments.create(content: 'comment content 2')
+  end
 
   describe '#comments_count' do
     it do
       expect(post.comments_count).to eq(post.comments.count)
+    end
+
+    it 'increment comments count' do
+      expect {
+        post.comments.create(content: 'new comment content')
+      }.to change {
+        post.comments_count
+      }.by(1)
+    end
+
+    it 'decrement comments count' do
+      expect {
+        post.comments.last.destroy
+      }.to change {
+        post.comments_count
+      }.by(-1)
     end
   end
 end
